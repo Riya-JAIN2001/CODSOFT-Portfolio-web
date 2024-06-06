28edf45dc7c8dec8299a1a0899121cc6a3e0b845
Riya-JAIN2001 <167556137+Riya-JAIN2001@users.noreply.github.com>
2024-06-06 11:24:03 +0530

    Add files via upload

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiSlack,
+  SiVercel,
+  SiMacos,
+} from "react-icons/si";
+import { SiReplit } from "react-icons/si";
+import { SiPycharm } from "react-icons/si";
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      {/* <Col xs={4} md={2} className="tech-icons">
+        <SiMacos />
+      </Col> */}
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+        <span style={{ fontSize: "12px", color: "white" }}> VS Code</span>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+        <span style={{ fontSize: "12px", color: "white" }}>Postman</span>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiReplit />
+        <span style={{ fontSize: "12px", color: "white" }}>Replit</span>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPycharm />
+        <span style={{ fontSize: "12px", color: "white" }}>PyCharm</span>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVercel />
+        <span style={{ fontSize: "12px", color: "white" }}>Vercel</span>
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
